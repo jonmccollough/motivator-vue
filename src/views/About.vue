07c10d88a4b8b71b1f2dcf3b43747240252dac79
Jon McCollough <jonathonmccollough@gmail.com>
2020-06-06 13:32:58 -0400

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <body>
+    <h1>
+        About the Motivator
+    </h1>
+    <container>
+      <p1 id=text>
+        This project started as a joke to help cheer up someone in my Tech Elevator class. This is now the third version. 
+        The Motivator v1 was a simple command line interface written using jave in eclipse. The Motivator v2 was an attempt to us SpringMVC
+        to integrate a database a seperate database with the intent to make it publicly accessable online. 
+        This is now the third attempt to write The Motivator. This time I did everything in Vue with the intention of making hosting it as simple
+        as possible. <br><br>
+      </p1>
+      <h4>
+        Welcome to The Motivator 3.1 <br><br>
+      </h4>
+      <p4>
+        (3.1 update introduced the navigation feature to include this About page)
+      </p4>
+      </container>
+    </body>
+  </div>
+</template>
+
+<style scoped>
+
+@media screen and (min-width: 900px) {
+    .container {
+      width: 50vw;
+    }
+}
+.container{
+  margin-right: 20px;
+}
+
+
+</style>
